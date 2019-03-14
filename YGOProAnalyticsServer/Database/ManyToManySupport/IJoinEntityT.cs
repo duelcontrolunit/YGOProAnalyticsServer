3018acbff4dad45ea33751e08d37537f5ee787ae
NightAngell <sobta24@wp.pl>
2019-03-14 19:25:25 +0100

    Added xmlDoc example for IJoinEntityT

--- YGOProAnalyticsServer/Database/ManyToManySupport/IJoinEntityT.cs
+++ YGOProAnalyticsServer/Database/ManyToManySupport/IJoinEntityT.cs
@@ -9,10 +9,35 @@
     public interface IJoinEntity<TEntity>
     {
         /// <summary>
-        /// Must be EXPLICITLY implemented. It should expose TEntity navigation property. For example:
-        /// <para>get => tEntity;</para>
-        /// <para>set => tEntity = value;</para>
+        /// Must be EXPLICITLY implemented. It should expose TEntity navigation property.
         /// </summary>
+        /// <example> 
+        ///     This sample shows how TEntity navigation properties should be exposed.
+        ///     <code>
+        ///         public class DbModelJoin : IJoinEntity&lt;FirstDbModel&gt;, IJoinEntity&lt;SecondDbModel&gt;
+        ///         {
+        ///             public int FirstDbModelId { get; set; }
+        ///             public FirstDbModel FirstDbModel { get; set; }
+        ///             
+        ///             //It is important to implement it explicitly.
+        ///             FirstDbModel IJoinEntity&lt;FirstDbModel&gt;.Navigation
+        ///             {
+        ///                 get => FirstDbModel;
+        ///                 set => FirstDbModel = value;
+        ///             }
+        ///             
+        ///             public int SecondDbModelId { get; set; }
+        ///             public SecondDbModel SecondDbModel { get; set; }
+        ///             
+        ///             //It is important to implement it explicitly.
+        ///             SecondDbModel IJoinEntity&lt;SecondDbModel&gt;.Navigation
+        ///             {
+        ///                 get => SecondDbModel;
+        ///                 set => SecondDbModel = value;
+        ///             }
+        ///         }
+        ///     </code>
+        /// </example>
         TEntity Navigation { get; set; }
     }
 }
