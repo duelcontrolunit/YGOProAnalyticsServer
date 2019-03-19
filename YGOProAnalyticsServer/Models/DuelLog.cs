5d08a4b5a0f448a3a653245bed59d219b64fe301
NightAngell <sobta24@wp.pl>
2019-03-19 15:35:34 +0100

    Created DuelLog model

---
+++ YGOProAnalyticsServer/Models/DuelLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YGOProAnalyticsServer.Models
+{
+    public class DuelLog
+    {
+        public int Id { get; protected set; }
+        public DateTime DateOfTheEndOfTheDuel { get; protected set; }
+        public int RoomId { get; protected set; }
+        public int RoomMode { get; set; }
+        public string Name { get; protected set; }
+        public string ReplayFilename { get; protected set; }
+        public List<string> DecksWhichWonFileNames { get; protected set; }
+        public List<string> DecksWhichLostFileNames { get; protected set; }
+    }
+}
