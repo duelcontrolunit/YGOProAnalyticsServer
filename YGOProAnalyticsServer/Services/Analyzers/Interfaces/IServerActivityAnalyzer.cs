c17b1e9eddaeca18e3e4da5e68a836792b291069
NightAngell <sobta24@wp.pl>
2019-04-30 12:52:57 +0200

    small change in xmlDocs

--- YGOProAnalyticsServer/Services/Analyzers/Interfaces/IServerActivityAnalyzer.cs
+++ YGOProAnalyticsServer/Services/Analyzers/Interfaces/IServerActivityAnalyzer.cs
@@ -14,36 +14,36 @@ namespace YGOProAnalyticsServer.Services.Analyzers.Interfaces
         /// Numbers the of games from last days.
         /// </summary>
         /// <param name="numberOfDays">The number of days.</param>
-        /// <returns>Number of games from last days.</returns>
+        /// <returns>Numbers the of games from last days.</returns>
         Task<int> NumberOfGamesFromLastDaysAsync(int numberOfDays);
 
         /// <summary>
-        /// Numbers the of games from month.
+        /// Numbers of the games from month.
         /// </summary>
         /// <param name="year">The year.</param>
         /// <param name="month">The month.</param>
-        /// <returns>Numbers the of games from month.</returns>
+        /// <returns>Numbers of the games from month.</returns>
         Task<int> NumberOfGamesFromMonthAsync(int year, int month);
 
         /// <summary>
-        /// Numbers the of games from one day.
+        /// Numbers of the games from one day.
         /// </summary>
         /// <param name="date">The date.</param>
-        /// <returns>Numbers the of games from one day.</returns>
+        /// <returns>Numbers of the games from one day.</returns>
         Task<int> NumberOfGamesFromOneDayAsync(DateTime date);
 
         /// <summary>
-        /// Numbers the of games from one day.
+        /// Numbers of the games from one day.
         /// </summary>
         /// <param name="duelLogsFromOneDay">The duel logs from one day.</param>
-        /// <returns>Numbers the of games from one day.</returns>
+        /// <returns>Numbers of the games from one day.</returns>
         int NumberOfGamesFromOneDay(IEnumerable<DuelLog> duelLogsFromOneDay);
 
         /// <summary>
-        /// Numbers the of games from one year.
+        /// Numbers of the games from one year.
         /// </summary>
         /// <param name="year">The year.</param>
-        /// <returns>Numbers the of games from one year.</returns>
+        /// <returns>Numbers of the games from one year.</returns>
         Task<int> NumberOfGamesFromOneYearAsync(int year);
     }
 }
