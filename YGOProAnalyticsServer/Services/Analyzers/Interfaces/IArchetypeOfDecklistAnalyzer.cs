920e016bc2ae6a3908f211b54a11b3ec924a526f
Szefo09 <szefo009@gmail.com>
2019-04-30 12:24:59 +0200

    small typo fixes

--- YGOProAnalyticsServer/Services/Analyzers/Interfaces/IArchetypeOfDecklistAnalyzer.cs
+++ YGOProAnalyticsServer/Services/Analyzers/Interfaces/IArchetypeOfDecklistAnalyzer.cs
@@ -11,7 +11,7 @@ namespace YGOProAnalyticsServer.Services.Analyzers.Interfaces
         /// <summary>
         /// Sets the decklist archetype from archetype cards used in it.
         /// </summary>
-        /// <param name="decklist">The decklist.</param>
+        /// <param name="decklist">The decklist (Must contain non empty list of cards in deck).</param>
         /// <returns>Decklist with archetype set</returns>
         Task<Decklist> SetDecklistArchetypeFromArchetypeCardsUsedInIt(Decklist decklist);
     }
